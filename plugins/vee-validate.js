40de2c7c37bf05e554563deea1bf129395298311
AmazingMoon <804721028@qq.com>
2018-10-16 23:22:59 +0800

    try to fix

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -3,7 +3,8 @@ import VeeValidate, { Validator } from 'vee-validate'
 import zh from 'vee-validate/dist/locale/zh_CN';//引入中文文件
 
 // 配置中文
-Validator.localize('zh', zh);
 Vue.use(VeeValidate, {
     inject: false // nuxt 与 vee-validate 不兼容，需要手动注入
-})
+});
+Validator.localize('zh', zh);
+
