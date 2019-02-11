91ba1e77a3b9c60196f25fb2f8f9cb52990a368d
AmazingMoon <804721028@qq.com>
2019-02-11 15:34:24 +0800

    modify style

--- components/app-header.vue
+++ components/app-header.vue
@@ -46,14 +46,11 @@
               </div>
               <div class="d-flex align-items-center" v-if="auth.id!=0">
                   <a href="/posts/create" target="_blank" role="button">
-                    <button class="btn btn-outline-secondary mr-2">
-                      <svg width="18" height="18" aria-hidden="true" data-prefix="fas" data-icon="edit" class="svg-inline--fa fa-edit fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path></svg>
-                      <span> 发布文章</span>
-                    </button>
+                    <button class="btn btn-outline-danger btn-sm mr-2">发布文章</button>
                   </a>
                 <div class="dropdown">
                   <a href="#" class="dropdown-toggle text-muted" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <hash-avatar :url="auth.avatar" :user_id="auth.id" :size=32 :alt="auth.name" class="rounded-circle"></hash-avatar>
+                    <hash-avatar :url="auth.avatar" :user_id="auth.id" :size=31 :alt="auth.name" class="rounded-circle"></hash-avatar>
                   </a>
                   <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
                     <router-link :to="'/users/' + auth.id" class="dropdown-item">
