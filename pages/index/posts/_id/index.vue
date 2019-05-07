3be49587cc47b7eaa75c248c108cb3c1cc889b0a
Amazing <804721028@qq.com>
2019-05-07 17:20:30 +0800

    add user avatar link

--- pages/index/posts/_id/index.vue
+++ pages/index/posts/_id/index.vue
@@ -65,7 +65,10 @@
           作者: {{post.user.name}}
         </div>
         <div class="d-flex justify-content-center my-3">
-          <hash-avatar :url="post.user.avatar" :user_id="post.user.id" :size=120 :alt="post.user.name" class="rounded-circle"></hash-avatar>
+          <router-link :to="'/users/' + this.post.user_id">
+            <hash-avatar :url="post.user.avatar" :user_id="post.user.id" :size=120 :alt="post.user.name"
+                         class="rounded-circle"></hash-avatar>
+          </router-link>
         </div>
         <div class="d-flex justify-content-center align-items-center text-muted" v-if="post.user.city">
           <svg width="16" height="16" aria-hidden="true" data-prefix="fas" data-icon="map-marker-alt" class="svg-inline--fa fa-map-marker-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg> <span>{{post.user.city}}</span>
