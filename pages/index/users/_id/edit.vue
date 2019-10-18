b8ee7da30c688df3c858d71cbb137bedc728328b
Amazing <804721028@qq.com>
2019-10-18 10:58:12 +0800

    add user edit page

---
+++ pages/index/users/_id/edit.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="edit-container p-3">
+        <div class="card text-center" style="width: 28rem">
+            <div class="card-body">
+                <div class="avatar-box" @mouseenter="isShowUploadText = true" @mouseleave="isShowUploadText = false">
+                    <img :src="user.avatar" :alt="user.name" class="rounded-circle">
+                    <div class="upload-text" v-if="isShowUploadText">点击上传</div>
+                    <input type="file" class="upload-file">
+                </div>
+                <input class="form-control mb-3 mt-3" type="text" placeholder="Default input" v-model.trim="user.name">
+                <button type="button" class="btn btn-primary" @click="updateInfo">确定</button>
+                <nuxt-link :to=" '/users/' + 7 " class="btn btn-secondary">取消</nuxt-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import HashAvatar from '~/components/hash-avatar'
+    import axios from 'axios'
+    import config from '~/config/api.js'
+
+    export default {
+        name: "user-details",
+        components: {
+            'hash-avatar': HashAvatar
+        },
+        data() {
+            return {
+                isShowUploadText: false,
+                user: {}
+            }
+        },
+        asyncData({params, error}) {
+            return axios.get(config.apiUrl + '/users/' + params.id)
+                .then((res) => {
+                    if (res.data.status === 0x010009) {
+                        error({statusCode: 404, message: '你找到这个人在地球上没有'})
+                    } else {
+                        return {user: res.data.data};
+                    }
+                }).catch(err => {
+                    error({statusCode: 500, message: '服务器挂了！赶快联系站长，13571899655@163.com'})
+                })
+        },
+        methods: {
+            updateInfo() {
+                this.$router.push('/users/' + this.user.id)
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .edit-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .avatar-box {
+        position: relative;
+        margin: 0 auto;
+        width: 100px;
+        height: 100px;
+    }
+
+    .rounded-circle {
+        width: 100px;
+    }
+
+    .upload-text {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 50px;
+        line-height: 45px;
+        background-color: #7F828B;
+        opacity: .8;
+        overflow: hidden;
+        z-index: 1;
+        border-bottom-left-radius: 50px;
+        border-bottom-right-radius: 50px;
+        color: #ffffff;
+    }
+
+    .upload-file {
+        z-index: 3;
+        opacity: 0;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100px;
+        height: 100px;
+        cursor: pointer;
+    }
+</style>
