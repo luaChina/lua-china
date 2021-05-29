45a40f7262c660f677231ce0b39e8a12fae3de75
贺钧威 <13571899655@163.com>
2021-05-29 23:25:17 +0800

    fix: index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -171,7 +171,7 @@ export default {
     },
 
     //2.x版本 asyncData不推荐使用callback方式
-    asyncData({params, error}) {
+    async asyncData({params, error}) {
         return axios.get(config.apiInternalUrl + '/posts')
             .then((res) => {
                 let num = Math.ceil(res.data.data.total / 20);
