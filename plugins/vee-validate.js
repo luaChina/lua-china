10bdabbfec0a2a85a54c5c07d71b732227e28915
Amazing <804721028@qq.com>
2021-11-04 15:11:17 +0800

    update vee-validate

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,10 +1,24 @@
-import Vue from 'vue'
-import VeeValidate, { Validator } from 'vee-validate'
-import zh from 'vee-validate/dist/locale/zh_CN';//引入中文文件
+import Vue from "vue";
+import {
+    ValidationProvider,
+    localize,
+    extend,
+    ValidationObserver
+} from "vee-validate";
+import zh_CN from "vee-validate/dist/locale/zh_CN.json";
+import {
+    required,
+    email,
+    numeric,
+    min,
+    confirmed
+} from "vee-validate/dist/rules";
 
-// 配置中文
-Vue.use(VeeValidate, {
-    inject: false // nuxt 与 vee-validate 不兼容，需要手动注入
-});
-Validator.localize('zh', zh);
-
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
+extend("email", email);
+extend("required", required);
+extend("numeric", numeric);
+extend("min", min);
+extend("confirmed", confirmed);
+localize("zh_CN", zh_CN);
